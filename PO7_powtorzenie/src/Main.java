a198987ba0753b8d6409b411d2d4e7ffff8037fb
ThePiedrita <118008754+ThePiedrita@users.noreply.github.com>
2023-11-24 09:56:57 +0100

    powtorzenie

--- PO7_powtorzenie/src/Main.java
+++ PO7_powtorzenie/src/Main.java
@@ -1,19 +1,15 @@
 import java.util.Arrays;
+import java.util.ArrayList;
+
 
 public class Main {
     public static void main(String[] args) {
-        /*int[] tab1 = {1, 2, 3, 4};
-        int[] tab2 = {5, 6, 7};
-        mergeArrays(tab1,tab2);*/
-
-        /*StringBuilder sb = new StringBuilder("jakiś napis bez dużych liter");
-        System.out.println(sb);
-        capitalizeEverySecond(sb);
-        System.out.println(sb);*/
-
-        nowaOsoba newPerson = new nowaOsoba();
-        System.out.println(newPerson.toString());
-
+        int[] tab1 = {1,2,3};
+        int[] tab2 = {4,5,6};
+        ArrayList<Integer> wynik = mergeArrays(tab1,tab2);
+        System.out.println(wynik);
+        StringBuilder zdanie = new StringBuilder("Ala3mA51oTa");
+        System.out.println(zdanie);
     }
     /*public static void mergeArrays(int[] tab1, int[] tab2){
         int[] merge = new int[tab1.length+ tab2.length];
@@ -31,6 +27,16 @@ public class Main {
             System.out.println(a);
         }
     }*/
+    public static ArrayList<Integer> mergeArrays(int[] tab1, int[] tab2) {
+        ArrayList<Integer> wynik = new ArrayList<>();
+        for (int i = 0; i < tab1.length; i++) {
+            wynik.add(tab1[i]);
+        }
+        for (int i = 0; i < tab2.length; i++) {
+            wynik.add(tab2[i]);
+        }
+        return wynik;
+    }
     /*public static void capitalizeEverySecond(StringBuilder sb){
         for(int i = 1; i < sb.length();i+=2){
             char znak = sb.charAt(i);
